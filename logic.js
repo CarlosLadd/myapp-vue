83773934ed6f9dcfec234e72817bf00b773a603d
CarlosLadd <carlosladdz@protonmail.com>
2020-10-05 11:42:27 -0600

    New examples added

--- logic.js
+++ logic.js
@@ -1,7 +1,42 @@
-var myAppTwo = new Vue({
-	el: '#toreto',
+// Example #1
+var exampleOne = new Vue({
+	el: '#exampleOne',
   data: {
   	exampleTwo: 'Hello World, Toreto',
     showWelcome: true
-  }
+  },
+	methods: {
+		didTapOnTestButton: function() {
+			if (this.showWelcome) {
+				this.showWelcome = false
+			} else {
+				this.showWelcome = true
+			}
+		}
+	}
+})
+
+// Example #2
+var exampleTwo = new Vue({
+	el: '#exampleTwo',
+	data: {
+		cars: [
+			{
+				name: "Toyota",
+				color: "Black"
+			},
+			{
+				name: "Nissan",
+				color: "Gray"
+			},
+			{
+				name: "BMW",
+				color: "Red"
+			},
+			{
+				name: "Mitsubishi",
+				color: "Blue"
+			}
+		]
+	}
 })
